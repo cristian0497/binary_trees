f8bee109ed18dca0f35980148d43b008d422ddc0
cristian0497 <cristian04.97@hotmail.com>
2020-03-10 17:51:46 +0000

    [ADD] betty OK, new node at the left.

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_insert_left - insert at left a new node
+ * @parent: parent of the new node
+ * @value: value to assign in the new node
+ * Return: New node at left
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (!parent->left)
+	{
+		parent->left = binary_tree_node(parent, value);
+		return (parent->left);
+	}
+	else
+	{
+		node = binary_tree_node(parent, value);
+		node->left = parent->left;
+		parent->left->parent = node;
+		parent->left = node;
+		return (node);
+	}
+
+}
