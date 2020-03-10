f33d4ec4f100624dbd23cbb0950f278b415d8e48
cristian0497 <cristian04.97@hotmail.com>
2020-03-10 19:08:31 +0000

    [ADD] Betty OK, insert a node at the right of tree structure

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - Insert a node at the right of tree
+ * @parent: tree structures to insert node
+ * @value: value to insert in a new node
+ * Return: a new node at the right
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (!parent->right)
+	{
+		parent->right = binary_tree_node(parent, value);
+		return (parent->left);
+	}
+	else
+	{
+		node = binary_tree_node(parent, value);
+		node->right = parent->right;
+		parent->right->parent = node;
+		parent->right = node;
+		return (node);
+	}
+
+}
