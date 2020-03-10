c106a045906e207a785e50e61ac5e91d917a7e85
cristian0497 <cristian04.97@hotmail.com>
2020-03-10 13:14:00 +0000

    [ADD] Betty OK, Create a new node, assign value and parent

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_node - A new node to the structure
+ * @parent: the parent node of the new node or NULL
+ * @value: Value int of Node
+ * Return: A new node, or NULL if fails
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	return (new);
+}
