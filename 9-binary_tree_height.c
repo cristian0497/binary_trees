05bdb113e2d70320edf6b97484b5a05f9545413a
cristian0497 <cristian04.97@hotmail.com>
2020-03-11 21:02:52 +0000

    [FIX] binary height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,13 +6,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l, r;
+	size_t l = 0, r = 0;
 
 	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
-	if (l > r)
+	if (l < r)
 		return (l + 1);
 	else
 		return (r + 1);
