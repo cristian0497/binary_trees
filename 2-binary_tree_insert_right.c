62ea4657ceb4cfacb527595bcf29082bbbd8f031
cristian0497 <cristian04.97@hotmail.com>
2020-03-11 19:05:33 +0000

    [FIX] if parent is NULL, return NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,6 +9,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (!parent)
+		return (NULL);
 	if (!parent->right)
 	{
 		parent->right = binary_tree_node(parent, value);
