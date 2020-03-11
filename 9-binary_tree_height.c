7f5ef7fb4e42522f8b8a412b2115b2c6d6f7617b
cristian0497 <cristian04.97@hotmail.com>
2020-03-11 20:07:01 +0000

    [ADD] hight of binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - the height of binary tree
+ * @tree: binary tree to check height
+ * Return: hight of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (!tree)
+		return (0);
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+	if (l > r)
+		return (l + 1);
+	else
+		return (r + 1);
+}
