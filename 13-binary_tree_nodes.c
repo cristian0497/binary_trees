502e7571b2caf5c3ee51154f70af121fbb5aeece
cristian0497 <cristian04.97@hotmail.com>
2020-03-12 04:45:08 +0000

    [ADD] Betty OK, print the numbers of child with almost 1 children

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - count the num of nodes with almost 1 children
+ * @tree: binary tree
+ * Return: num of nodes with almost 1 children
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+	{
+		nodes = binary_tree_nodes(tree->left);
+		nodes = binary_tree_nodes(tree->right);
+		if (tree->left && tree->right)
+			nodes++;
+	}
+	return ((tree->left || tree->right) ? nodes + 1 : nodes);
+
+}
